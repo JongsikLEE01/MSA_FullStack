4aad674c30b918d766e90dee0183578b3aa9d4c5
JongsikLEE01 <137877490+JongsikLEE01@users.noreply.github.com>
2024-06-11 17:05:53 +0900

    React 프로젝트 생성까지

---
+++ REACT/START/start-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+// 컴포넌트 정의
+function App() {
+  return (
+    <div>
+      <h1>hello, React</h1>
+      <h2>hello, React</h2>
+      <p>JSX를 사용한 React 컴포넌트</p>
+    </div>
+  );
+}
+
+// 모듈 exprot
+export default App;
---
+++ REACT/START/start-app/src/Header.js
@@ -0,0 +1,14 @@
+// 컴포넌트 정의
+// 컴포넌트 exprot
+export function Header() {
+    return (
+      <header>
+        <ul>
+            <a href="#"><img alt="/logo192.png"></img></a>
+            <li>드롭다운</li>
+            <li>드롭다운</li>
+            <li>드롭다운</li>
+        </ul>
+      </header>
+    );
+  }
